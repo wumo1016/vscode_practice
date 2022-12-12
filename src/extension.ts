5fea73fb5fa893388e8e04b5e5ed3f27f936bbe3
wumo1016 <760478279@qq.com>
2022-12-12 16:29:36 +0800

    chore: test版本

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,8 @@
 import * as vscode from 'vscode'
-import { getFunctionCode } from './main'
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('wumo.helloWorld', () => {
-    vscode.window.showInformationMessage('执行啦')
-
-    // 删除字符
-    const editor = vscode.window.activeTextEditor
-    if (!editor) return
-
-    const code = editor.document.getText()
-    const index = editor.document.offsetAt(editor.selection.active)
-    const functionNode = getFunctionCode(index, code)
-    if (!functionNode) return
-    const { start, end } = functionNode
-
-    editor.edit((editBuilder) => {
-      editBuilder.delete(
-        new vscode.Range(
-          new vscode.Position(start.line - 1, start.column),
-          new vscode.Position(end.line - 1, end.column)
-        )
-      )
-    })
+  let disposable = vscode.commands.registerCommand('wyb.helloWorld', () => {
+    vscode.window.showInformationMessage('wyb 执行啦!')
   })
 
   context.subscriptions.push(disposable)
