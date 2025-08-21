997e7ce6fe7c04545900833641f5418049f5294d
wumo <760478279@qq.com>
2025-08-21 13:24:57 +0800

    chore: 测试翻译插件

--- wumo-trans/src/extension.ts
+++ wumo-trans/src/extension.ts
@@ -1,15 +1,39 @@
-/*
- * @Description:
- * @Author: wyb
- * @LastEditors: wyb
- * @LastEditTime: 2024-04-21 21:07:16
- */
-import { ExtensionContext, window } from 'vscode'
-import { Sidebar } from './sidebar'
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode'
+import { SideBarEntry } from './test';
 
-export function activate(context: ExtensionContext) {
-  const sidebar = new Sidebar('Chinese-List', context)
-  window.registerTreeDataProvider(sidebar.id, sidebar)
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "wumo-trans" is now active!')
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'wumo-trans.wumoTest',
+    () => {
+      // The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage('Hello World from wumo-trans!')
+    }
+  )
+
+  
+  context.subscriptions.push(disposable)
+
+  const sidebarArvinjunGeneral = new SideBarEntry('Arvinjun-General', context);
+  vscode.window.registerTreeDataProvider(
+    sidebarArvinjunGeneral.id,
+    sidebarArvinjunGeneral
+  );
+
 }
 
+// This method is called when your extension is deactivated
 export function deactivate() {}
+
+
